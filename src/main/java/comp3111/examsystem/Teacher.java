75aa4742f9a32c3af6687793dfd435f82672b569
WaiKinTANG <wk.tang@protonmail.com>
2024-10-29 14:45:13 +0800

    Created Class Teacher.

---
+++ src/main/java/comp3111/examsystem/Teacher.java
@@ -0,0 +1,57 @@
+package comp3111.examsystem;
+
+public class Teacher extends User {
+    public Teacher(String username, String password, String name, String gender, int age, String department) {
+        super(username, password, name, gender, age, department);
+    }
+
+    public void createExam() {
+        // TODO: implement
+    }
+
+    public void viewExam() {
+        // TODO: implement
+    }
+
+    public void deleteExam() {
+        // TODO: implement
+    }
+
+    public void updateExam() {
+        // TODO: implement
+    }
+
+    public void viewStudent() {
+        // TODO: implement
+    }
+
+    public void viewStudentAnswer() {
+        // TODO: implement
+    }
+
+    public void gradeStudentAnswer() {
+        // TODO: implement
+    }
+
+    public void viewQuestion() {
+        // TODO: implement
+    }
+
+    public void createQuestion() {
+        // TODO: implement
+    }
+
+    public void deleteQuestion() {
+        // TODO: implement
+    }
+
+    public void updateQuestion() {
+        // TODO: implement
+    }
+
+    public void viewQuestionBank() {
+        // TODO: implement
+    }
+
+
+}
