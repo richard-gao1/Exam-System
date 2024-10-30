82b00b7234670baa9254b3cb4654212786e88788
Richard Gao <richard.g2002@gmail.com>
2024-10-30 16:37:22 +0800

    Make skeleton code for Backend
    Added in basic skeletons of all required classes for backend.

--- src/main/java/comp3111/examsystem/User.java
+++ src/main/java/comp3111/examsystem/User.java
@@ -1,46 +1,17 @@
 package comp3111.examsystem;
 
-public class User {
-    private String username;
-    private String password;
+public class User extends Account{
     private String name;
     private String gender;
     private int age;
     private String department;
 
     public User(String username, String password, String name, String gender, int age, String department ) {
-        this.username = username;
-        this.password = password;
+        super(username, password);
         this.name = name;
         this.gender = gender;
         this.age = age;
         this.department = department;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        // check if valid username (will need to verify with database eventually)
-        if (username != "") {
-            this.username = username;
-        } else {
-            throw new IllegalArgumentException("Username cannot be empty");
-        }
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        // can add password requirements here
-        if (password != "") {
-            this.password = password;
-        } else {
-            throw new IllegalArgumentException("Password cannot be empty");
-        }
     }
 
     public String getName() {
@@ -48,7 +19,7 @@ public class User {
     }
 
     public void setName(String name) {
-        if (name != "") {
+        if (name.isEmpty()) {
             this.name = name;
         } else {
             throw new IllegalArgumentException("Name cannot be empty");
