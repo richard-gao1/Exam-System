eeb22ea582245444649e3f6c8d14b884a23113d8
Richard Gao <richard.g2002@gmail.com>
2024-10-30 16:39:42 +0800

    Squash

---
+++ src/main/java/comp3111/examsystem/Account.java
@@ -0,0 +1,37 @@
+package comp3111.examsystem;
+
+public class Account {
+    private String username;
+    private String password;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        // check if valid username (will need to verify with database eventually)
+        if (username.isEmpty()) {
+            this.username = username;
+        } else {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        // can add password requirements here
+        if (!password.isEmpty()) {
+            this.password = password;
+        } else {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+    }
+}
---
+++ src/main/java/comp3111/examsystem/Manager.java
@@ -0,0 +1,12 @@
+package comp3111.examsystem;
+
+public class Manager extends Account{
+    public Manager(String username, String password) {
+        super(username, password);
+    }
+
+    public Course makeCourse() {
+        // TODO: to be implemented
+        return null;
+    }
+}
