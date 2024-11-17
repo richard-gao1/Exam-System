5becbcc8cb00e5995da04082e652ef041bfb6d23
WaiKinTANG <wk.tang@protonmail.com>
2024-11-18 03:17:55 +0800

    Updated Exam, Question for UI purpose

--- src/main/java/comp3111/examsystem/SystemDatabase.java
+++ src/main/java/comp3111/examsystem/SystemDatabase.java
@@ -18,6 +18,9 @@ public class SystemDatabase {
     static final String data_filetype = ".json";
     public static User currentUser;
 
+    static public User currentUser = null;
+
+
     private boolean createFolder(String directory) {
         File folder = new File(directory);
         if (!folder.exists()) return folder.mkdir();
@@ -479,4 +482,6 @@ public class SystemDatabase {
         writeCourseFile(course);
         return "";
     }
+
+
 }
