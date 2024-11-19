9b72b4bfc9ac1074f59c545df7437f39640429be
whwmaust2125 <wesleymahk@protonmail.com>
2024-11-19 14:48:33 +0800

    File delete function

--- src/main/java/comp3111/examsystem/SystemDatabase.java
+++ src/main/java/comp3111/examsystem/SystemDatabase.java
@@ -1,6 +1,7 @@
 package comp3111.examsystem;
 
 import com.google.gson.Gson;
+import org.apache.commons.io.FileUtils;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -15,9 +16,6 @@ public class SystemDatabase {
 
     // maps Username -> Instance of Account
     static final String data_filetype = ".json";
-
-    static public User currentUser = null;
-
 
     private boolean createFolder(String directory) {
         File folder = new File(directory);
@@ -42,7 +40,11 @@ public class SystemDatabase {
     }
 
     public static void removeAll() {
-        removeFile("data");
+        try {
+            FileUtils.deleteDirectory(new File("data"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public SystemDatabase() {
@@ -475,6 +477,4 @@ public class SystemDatabase {
         writeCourseFile(course);
         return "";
     }
-
-
 }
